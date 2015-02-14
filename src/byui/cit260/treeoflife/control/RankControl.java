f093e39ca741d3b9b5cc2e920f474d7e0c825cb4
Erickson <Erickson@10.0.0.4>
2015-02-13 20:42:07 -0800

    control to test

--- src/byui/cit260/treeoflife/control/RankControl.java
+++ src/byui/cit260/treeoflife/control/RankControl.java
@@ -17,7 +17,7 @@ public class RankControl {
     }
       return false; 
     }     
-           
+   
 }        
         
         
--- test/byui/cit260/treeoflife/control/RankControlTest.java
+++ test/byui/cit260/treeoflife/control/RankControlTest.java
@@ -5,6 +5,8 @@
  */
 package byui.cit260.treeoflife.control;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,6 +18,14 @@ public class RankControlTest {
     
     public RankControlTest() {
     }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
 
     /**
      * Test of salvationEligible method, of class RankControl.
@@ -23,32 +33,14 @@ public class RankControlTest {
     @Test
     public void testSalvationEligible() {
         System.out.println("salvationEligible");
-        
-        System.out.println("\tTest case #1");
-        double value = 7.0;
-        double thresholdValue = 7.0;
+        double value = 0.0;
+        double thresholdValue = 0.0;
         RankControl instance = new RankControl();
         boolean expResult = true;
         boolean result = instance.salvationEligible(value, thresholdValue);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
-        
-        
-       
     }
-    {
-      System.out.println("\tTest case #2");
-        
-        double value = 1.0;
-        double thresholdValue = 7.0;
-        RankControl instance = new RankControl();
-        boolean expResult = false;
-        boolean result = instance.salvationEligible(value, thresholdValue);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-        
     
-}
 }
