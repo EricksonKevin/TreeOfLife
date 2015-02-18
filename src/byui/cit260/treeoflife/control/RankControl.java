0a3d7331d37f15226b46f37a3b8c98916a0af7bd
Erickson <Erickson@10.0.0.4>
2015-02-17 21:04:34 -0800

    RankControl salvation and faith tests

--- src/byui/cit260/treeoflife/control/RankControl.java
+++ src/byui/cit260/treeoflife/control/RankControl.java
@@ -12,28 +12,28 @@ package byui.cit260.treeoflife.control;
 public class RankControl {
     
     public boolean  salvationEligible(double value, double thresholdValue){
-      if (value <= thresholdValue ) {
+      if (value >= thresholdValue ) {
         return true;
     }
       return false; 
     }     
    
     public boolean  faithEligible(double value, double thresholdValue){
-      if (value <= thresholdValue ) {
+      if (value >= thresholdValue ) {
         return true;
     }
       return false; 
     } 
     
     public boolean  spiritEligible(double value, double thresholdValue){
-      if (value <= thresholdValue ) {
+      if (value >= thresholdValue ) {
         return true;
     }
       return false; 
     } 
     
     public boolean  rightEligible(double value, double thresholdValue){
-      if (value <= thresholdValue ) {
+      if (value >= thresholdValue ) {
         return true;
     }
       return false; 
--- test/byui/cit260/treeoflife/control/RankControlTest.java
+++ test/byui/cit260/treeoflife/control/RankControlTest.java
@@ -32,30 +32,148 @@ public class RankControlTest {
      */
     @Test
     public void testSalvationEligible() {
-        System.out.println("salvationEligible");
-        double value = 6.0;
+        System.out.println("salvationEligible_test1");
+        double value = 7.0;
         double thresholdValue = 7.0;
         RankControl instance = new RankControl();
         boolean expResult = true;
         boolean result = instance.salvationEligible(value, thresholdValue);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        
+        
+    }
+{
+        System.out.println("salvationEligible_test2");
+        double value = 9.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = true;
+        boolean result = instance.salvationEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
         
     }
 
+{
+        System.out.println("salvationEligible_test3");
+        double value = 1.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = false;
+        boolean result = instance.salvationEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
+        
+    }
+
+{
+        System.out.println("salvationEligible_test4");
+        double value = 6.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = false;
+        boolean result = instance.salvationEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
+        
+    }
+
+{
+        System.out.println("salvationEligible_test5");
+        double value = 7.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = true;
+        boolean result = instance.salvationEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
+        
+    }
+
+{
+        System.out.println("salvationEligible_test6");
+        double value = 10.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = true;
+        boolean result = instance.salvationEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
+        
+    }
     /**
      * Test of faithEligible method, of class RankControl.
      */
     @Test
     public void testFaithEligible() {
-        System.out.println("faithEligible");
+        System.out.println("faithEligible_test1");
         double value = 7.0;
         double thresholdValue = 7.0;
         RankControl instance = new RankControl();
         boolean expResult = true;
         boolean result = instance.faithEligible(value, thresholdValue);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        
+       
+    }
+    
+    {
+        System.out.println("faithEligible_test2");
+        double value = 9.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = true;
+        boolean result = instance.faithEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
+       
+    }
+    
+    {
+        System.out.println("faithEligible_test3");
+        double value = 1.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = false;
+        boolean result = instance.faithEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
+       
+    }
+    
+    {
+        System.out.println("faithEligible_test4");
+        double value = 6.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = false;
+        boolean result = instance.faithEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
+       
+    }
+    
+    {
+        System.out.println("faithEligible_test5");
+        double value = 7.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = true;
+        boolean result = instance.faithEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
+       
+    }
+    
+    {
+        System.out.println("faithEligible_test6");
+        double value = 10.0;
+        double thresholdValue = 7.0;
+        RankControl instance = new RankControl();
+        boolean expResult = true;
+        boolean result = instance.faithEligible(value, thresholdValue);
+        assertEquals(expResult, result);
+        
        
     }
 
@@ -65,13 +183,13 @@ public class RankControlTest {
     @Test
     public void testSpiritEligible() {
         System.out.println("spiritEligible");
-        double value = 8.0;
+        double value = 7.0;
         double thresholdValue = 7.0;
         RankControl instance = new RankControl();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.spiritEligible(value, thresholdValue);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        
         
     }
 
@@ -84,10 +202,10 @@ public class RankControlTest {
         double value = 9.0;
         double thresholdValue = 7.0;
         RankControl instance = new RankControl();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.rightEligible(value, thresholdValue);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        
         
     }
     
