5f3490d4c35c4e69474bbdb8189cd1a605707ade
Erickson <Erickson@10.0.0.4>
2015-02-21 06:54:50 -0800

    view class

---
+++ src/byui/cit260/treeoflife/view/StartProgramView.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.treeoflife.view;
+
+/**
+ *
+ * @author Erickson
+ */
+public class StartProgramView {
+    
+
+    public StartProgramView() {
+}
+
+    public void startProgram(){
+        
+        //Display the banner screen
+        this.displayBanner();
+        
+        //Prompt the player to enter their name.  Retrieve the name of player
+        
+        //Create and save player object
+        
+        //Display personalized welcome message
+        
+        //Display the Main menu
+
+}
+
+    private void displayBanner() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}   
