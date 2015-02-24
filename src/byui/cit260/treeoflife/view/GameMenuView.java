3f26afdc1b742e66645f0a37b70c5e5a65ac68ba
Nexeus <Nexeus@Nexeus-PC>
2015-02-23 20:21:00 -0700

    View week 7 commit

--- src/byui/cit260/treeoflife/view/GameMenuView.java
+++ src/byui/cit260/treeoflife/view/GameMenuView.java
@@ -5,15 +5,93 @@
  */
 package byui.cit260.treeoflife.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Erickson
  */
 public class GameMenuView {
 
+        public final String MENU = "\n"
+            + "\n---------------------------------------"
+            + "\n| Character Selection                           |"
+            + "\n---------------------------------------"
+            + "\nN - Nephi"
+            + "\nS - Sariah"
+            + "\nL - Laman"
+            + "\nQ - Quit"
+            + "\n---------------------------------------";
+        
     void displayMenu() {
-        System.out.println("\n*** displayMenu stub function called ***");
+        
+        char selection = ' ';
+        do {
+            System.out.println(MENU);  //display the main menu
+            
+            String input = this.getInput(); // get the users selection
+            selection = input.charAt(0); //get first character of string
+            
+            this.doAction(selection); //do action based on selection
+            
+        } while (selection != 'Q'); // a selection is not "Exit"
+    }
+
+    private String getInput() {
+       Boolean valid = false; //indicate if the name has been retrieved
+            String choice = null;
+            Scanner keyboard = new Scanner(System.in);//keyboard input stream
+            
+            while(!valid){//while a valid selection has not been retrieved
+                //prompt for the player's selection
+                System.out.println("Select Option");
+                // get the selection from the keyboard and trim off the blanks
+                choice = keyboard.nextLine();
+                choice = choice.trim();
+                
+                break; // out of the (exit) the repetition
+            }
+            return choice; // return the selection option
+    }
+
+    private void doAction(char choice) {
+        
+        switch (choice){
+            case 'N': // Goal of the game
+                this.startNephiGame();
+                break;
+            case 'S': //How to move
+                this.startSariahGame();
+                break;
+            case 'L': //Character Status
+                 this.startLamanGame();
+                break;
+            case 'Q': //Wuit help menu
+                return;
+            default:
+                System.out.println("\n***Invalid selection *** Try again");
+                break;
+                
+               
+        }
+    }
+
+    private void startNephiGame() {
+        //display the Nephi menu
+      NephiGameView gameMenu = new NephiGameView();
+      gameMenu.displayMenu();
+    }
+
+    private void startSariahGame() {
+        SariahGameView gameMenu = new SariahGameView();
+      gameMenu.displayMenu();
+    }
+
+    private void startLamanGame() {
+        LamanGameView gameMenu = new LamanGameView();
+      gameMenu.displayMenu();
+    }
         
     }
     
-}
+
