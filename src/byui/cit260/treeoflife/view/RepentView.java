3075dd7225ab18d9e3d4f5a85000ca6c1e35fdc7
Erickson <Erickson@10.0.0.4>
2015-03-06 18:39:49 -0800

    Repent View

---
+++ src/byui/cit260/treeoflife/view/RepentView.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.treeoflife.view;
+
+import byui.cit260.treeoflife.control.GameControl;
+import kevinjoshtreeoflife.KevinJoshTreeOfLife;
+
+/**
+ *
+ * @author Erickson
+ */
+public class RepentView extends View {
+    public RepentView() {
+        super("\n"
+            + "\n---------------------------------------"
+            + "\n| The Lord is pleased with your       |"
+            + "\n| decision to repent, and He knows    |"
+            + "\n| that you will find happiness in     |"
+            + "\n| your choice to follow the Lord.     |"
+            + "\n|                                     |"
+            + "\n| Yea, I would declare unto every     |"
+            + "\n| soul, as with the voice of thunder, |"
+            + "\n| repentance and the plan of          |"
+            + "\n| redemption, that they should repent |"
+            + "\n| and come unto our God, that there   |"
+            + "\n| might not be more sorrow upon all   |"
+            + "\n| the face of the earth.              |"
+            + "\n|     Alma 29:2                       |"
+            + "\n|                                     |"
+            + "\n|Please take a moment to ponder your  |"
+            + "\n|transgression, and fully commit      |"
+            + "\n|yourself to this repentance process. |"
+            + "\n---------------------------------------"
+            + "\nS - Repent for Salvation points"
+            + "\nF - Repent for Faith points"
+            + "\nP - Repent for Spirit points"
+            + "\nR - Repent for Righteousness points"
+            + "\nE - Exit"
+            + "\n---------------------------------------");
+    }  
+    
+    @Override
+    public void doAction(Object obj) {
+        
+        String value  = (String)obj;
+        
+        char choice = value.charAt(0);
+        switch (choice){
+            case 'S': // Repent for Salvation points
+                System.out.println("\n*You earned 1 Salvation point*");
+                break;
+            case 'F': // Repent for Faith points
+                System.out.println("\n*You earned 1 Faith point*");
+                break;
+            case 'P': // Repent for Spirit points
+                System.out.println("\n*You earned 1 Spirit point*");
+                break;
+            case 'R': // Repent for Righteousness points
+                System.out.println("\n*You earned 1 Righteousness point*");
+                break;  
+            case 'E': //exit the program
+                return ;
+                
+            default:
+                System.out.println("\n***Invalid selection *** Try again");
+                break;
+                
+               
+        }
+    }
+}
+
+   
