8198a63bd6d44969581e759d58851c7757a9e40c
Erickson <Erickson@10.0.0.4>
2015-03-19 21:43:14 -0700

    ViewLayerException.java

---
+++ src/byui/cit260/treeoflife/exceptions/ViewLayerException.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.treeoflife.exceptions;
+
+/**
+ *
+ * @author Erickson
+ */
+public class ViewLayerException extends Exception{
+
+    public ViewLayerException() {
+    }
+
+    public ViewLayerException(String message) {
+        super(message);
+    }
+
+    public ViewLayerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ViewLayerException(Throwable cause) {
+        super(cause);
+    }
+
+    public ViewLayerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    
+}
